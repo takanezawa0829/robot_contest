0813c672c246923bccd0937a0fc09cf622bbbae0
TakumuTakanezawa <takanen20010829kota@gmail.com>
2022-05-05 15:21:37 +0900

    fix

---
+++ scripts/output_csv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# license removed for brevity
+# import rospy
+# from robot_contest.msg import OutputCsv
+
---
+++ scripts/publish_sample.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# license removed for brevity
+# import rospy
+# from robot_contest.msg import OutputCsv
