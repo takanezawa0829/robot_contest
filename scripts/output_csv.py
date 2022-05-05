6a0156cef2aa6810622fdf5320fb625012925126
TakumuTakanezawa <takanen20010829kota@gmail.com>
2022-05-05 18:39:11 +0900

    add output_csv.py

--- scripts/output_csv.py
+++ scripts/output_csv.py
@@ -1,6 +1,92 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # license removed for brevity
-# import rospy
-# from robot_contest.msg import OutputCsv
+import rospy
+from robot_contest.msg import OutputCsv
+# ディレクトリを操作するためのモジュールをimport
+import os
+# csvファイルを操作するためのモジュールをimpot
+import csv
+# 時刻を取得するためのモジュールをimport
+import datetime
+
+# csvファイルをlogディレクトリに作成する関数（ファイル名は実行した時刻）
+def make_csv():
+    global now_str
+    # logディレクトリが存在していなければ新たに作成
+    if not os.path.exists('./log/'):
+        os.mkdir('log')
+    # ファイルを作成
+    with open(f'log/{now_str}.csv', 'w') as csv_file:
+        csv_file.write('')
+
+# csvファイルにヘッダーを入力する関数
+def output_header(header):
+    global now_str
+    with open(f'log/{now_str}.csv', 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+        writer.writeheader()
+
+
+# csvファイルにデータを入力する関数
+def output_data(header, data):
+    global now_str
+    with open(f'log/{now_str}.csv', 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+        writer.writerow(data)
+
+# 最初に呼ばれた時だけtrueを返す関数
+def getFlag():
+    flag = getFlag.flag
+    getFlag.flag = False
+    return flag
+getFlag.flag = True
+
+# Subscribeする対象のトピックが更新されたら呼び出されるコールバック関数
+# 引数にはトピックにPublishされるメッセージの型と同じ型を定義
+def callback(data):
+    # ヘッダーをheader_listに代入
+    header_list = []
+    for header in data.header:
+        header_list.append(header)
+
+    # データをrowに代入
+    value = {}
+    for i in range(len(data.header)):
+        value[data.header[i]] = data.value[i]
+
+    # 初めの1度だけ実行
+    if getFlag():
+        output_header(header_list)
+
+    # 受け取ったデータをコンソールに出力
+    print(value)
+
+    # 受け取ったデータをcsvに出力
+    output_data(header_list, value)
+
+
+
+def output_csv():
+    rospy.init_node('output_csv', anonymous=True)
+
+    # 現在時刻を取得
+    now = datetime.datetime.now()
+    global now_str
+    now_str = now.strftime('%Y_%m_%d_%H_%M_%S')
+
+    # csvファイルを作成
+    make_csv()
+
+    # Subscriberとしてoutput_csvというトピックに対してSubscribeし、topicが更新されたときはcallbackという名前のコールバック関数を実行
+    rospy.Subscriber('output_csv', OutputCsv, callback)
+
+    # topic更新の待ちうけを行う関数
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    # 実際にsubscribeする関数を呼び出し
+    output_csv()
+
 
--- scripts/publish_sample.py
+++ scripts/publish_sample.py
@@ -1,5 +1,56 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # license removed for brevity
-# import rospy
-# from robot_contest.msg import OutputCsv
+
+# output_csvにデータをpublishするサンプルプログラム
+
+# pythonでROSのソフトウェアを記述するときにimportするモジュール
+from os import rename
+import rospy
+# 自分で定義したmessageファイルから生成されたモジュール
+from robot_contest.msg import OutputCsv
+
+# 実際にpublishする関数
+def publish_date():
+    # 初期化宣言 : このソフトウェアは"publish_date"という名前
+    rospy.init_node('publish_date', anonymous=True)
+
+    # nodeの宣言: publisherのインスタンスを作る
+    # output_csvというtopicにOutputCsv型のmessageを送るPublisherを作成
+    oc = rospy.Publisher('output_csv', OutputCsv, queue_size=100)
+
+    # 1秒間にpublishする数の設定
+    r = rospy.Rate(10)
+
+    # 送信するデータxを定義する
+    x = 0
+
+    # OutputCsv型のmessageのインスタンスを作成
+    oc_msg = OutputCsv()
+
+    # ヘッダーを設定
+    oc_msg.header = ["項目1", "項目2", "項目3", "項目4", "項目5"]
+
+    # ctl + Cで終了しない限りwhileループでpublishし続ける
+    while not rospy.is_shutdown():
+        # データを入力
+        oc_msg.value = []
+        for i in range(5):
+            oc_msg.value.append(x)
+            x += 1
+
+        print(oc_msg.value)
+
+        # publishする関数
+        oc.publish(oc_msg)
+
+        # rだけ待機
+        r.sleep()
+
+
+# コマンドプロンプトから呼び出された時だけ関数を実行するためのif文
+if __name__ == '__main__':
+    try:
+        publish_date()
+
+    except rospy.ROSInterruptException: pass
