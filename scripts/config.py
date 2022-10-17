ff99ee3c238d6f972b151157c5056abdd08a7360
TakumuTakanezawa <takanen20010829kota@gmail.com>
2022-10-17 19:33:12 +0900

    fix machine_interface.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,42 +1,60 @@
 import numpy as np
 
+# uart通信のポート設定
 serial_port = '/dev/ttyUSB0'
 serial_timeout = 1
 
-count = 0
-
-# 数値データが1増えると何度増えるか
+# サーボの数値データが1増えると何度増えるか
 data_rad_diff = 0.24
 
+# サーボを制御するための設定
 foot = {
+    # 前脚
     'front': {
         'left': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [1, 2, 3], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev1', 'rev2', 'rev3'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
         'right': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [4, 5, 6], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev4', 'rev5', 'rev6'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
     },
+    # 真ん中の脚
     'middle': {
         'left': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [7, 8, 9], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev7', 'rev8', 'rev9'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
         'right': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [10, 11, 12], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev10', 'rev11', 'rev12'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
     },
+    # 後ろ脚
     'end': {
         'left': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [13, 14, 15], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev13', 'rev14', 'rev15'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
         'right': {
-            'standard': [500, 500, 200],
-            'reverse': [True, True, False],
+            'id': [16, 17, 18], # サーボのID [第1関節, 第2関節, 第3関節]
+            'rev': ['rev16', 'rev17', 'rev18'], # urdfのjointの名前 [第1関節, 第2関節, 第3関節]
+            'standard': [500, 500, 200], # 関節角度が0度のときのサーボモータの値 [500, 500, 200]付近を推薦
+            'reverse': [True, True, False], # サーボの回転方向を逆にするか [第1関節, 第2関節, 第3関節]
         },
     },
 }
+
+# Publishされたjoint_statesの回数を数えるために使用(変更禁止)
+count = 0
--- scripts/machine_interface.py
+++ scripts/machine_interface.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # license removed for brevity
-from turtle import pos
+
 import rospy
 from sensor_msgs.msg import JointState
 import HiwonderServoController as servo
 import config
 servo.setConfig(config.serial_port, config.serial_timeout)
 import numpy as np
-import math
 
 def deg2data(deg, standard, reverse):
     if(reverse):
@@ -24,22 +23,60 @@ def data2deg(data, standard, reverse):
 # 引数にはトピックにPublishされるメッセージの型と同じ型を定義
 def callback(data):
     # ここで実際にサーボを動作させる
-    position = np.rad2deg(data.position)
-    position = np.array(position)
-    front_left_foot = [position[0], position[1], position[2]]
-    print(front_left_foot)
-    front_left_foot = [
-        deg2data(front_left_foot[0], 500, True),
-        deg2data(front_left_foot[1], 500, True),
-        deg2data(front_left_foot[2], 200, False),
-    ]
-    front_left_foot = np.array(front_left_foot).astype(int)
-    servo.moveServo(1, front_left_foot[0], 500)
-    servo.moveServo(2, front_left_foot[1], 500)
-    servo.moveServo(3, front_left_foot[2], 500)
+    for i in np.arange(len(data.name)):
+        # dummy jointを除外
+        if data.name[i] in ang_list:
+            id = ang_list[data.name[i]]['id']
+            standard = ang_list[data.name[i]]['standard']
+            reverse = ang_list[data.name[i]]['reverse']
+            deg = np.rad2deg(data.position[i])
+            print('joint名', data.name[i])
+            print('id', id)
+            print('角度[deg]', deg)
+            print('standard', standard)
+            print('reverse', reverse)
+            ang_data = deg2data(deg, standard, reverse)
+            print('ang_data', ang_data)
+            # サーボを動作
+            servo.moveServo(id, ang_data, standard)
 
 def serial():
     rospy.init_node('machine_interface', anonymous=True)
+
+    global ang_list
+    ang_list = {}
+    foot = config.foot
+    for i in np.arange(3):
+        ang_list[foot['front']['left']['rev'][i]] = {
+            'id': foot['front']['left']['id'][i],
+            'standard': foot['front']['left']['standard'][i],
+            'reverse': foot['front']['left']['reverse'][i],
+        }
+        ang_list[foot['front']['right']['rev'][i]] = {
+            'id': foot['front']['right']['id'][i],
+            'standard': foot['front']['right']['standard'][i],
+            'reverse': foot['front']['right']['reverse'][i],
+        }
+        ang_list[foot['middle']['left']['rev'][i]] = {
+            'id': foot['middle']['left']['id'][i],
+            'standard': foot['middle']['left']['standard'][i],
+            'reverse': foot['middle']['left']['reverse'][i],
+        }
+        ang_list[foot['middle']['right']['rev'][i]] = {
+            'id': foot['middle']['right']['id'][i],
+            'standard': foot['middle']['right']['standard'][i],
+            'reverse': foot['middle']['right']['reverse'][i],
+        }
+        ang_list[foot['end']['left']['rev'][i]] = {
+            'id': foot['end']['left']['id'][i],
+            'standard': foot['end']['left']['standard'][i],
+            'reverse': foot['end']['left']['reverse'][i],
+        }
+        ang_list[foot['end']['right']['rev'][i]] = {
+            'id': foot['end']['right']['id'][i],
+            'standard': foot['end']['right']['standard'][i],
+            'reverse': foot['end']['right']['reverse'][i],
+        }
 
     # Subscriberとしてrobot_joint_dataというトピックに対してSubscribeし、topicが更新されたときはcallbackという名前のコールバック関数を実行
     rospy.Subscriber('robot_joint_data', JointState, callback)
